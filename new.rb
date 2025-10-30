db3b77fa27040d1a084a7d783c55219ff3efa4e2
mohammedsbd <mohammed1112ok@gmail.com>
2025-10-30 11:24:24 -0700

    Update new.rb

--- new.rb
+++ new.rb
@@ -10771,14 +10771,7 @@ class CreateNotes < ActiveRecord::Migration[7.0]
       t.boolean :pinned, null: false, default: false
       t.timestamps
     end
-    add_index :notes, :created_at
-  end
-end
-
-    add_index :notes, :pinned
-    add_index :notes, :created_at
-  end
-end
+ 
   def change
     create_table :notes do |t|
       t.string :title, null: false
@@ -10812,6 +10805,7 @@ class CreateNotes < ActiveRecord::Migration[7.0]
       t.boolean :pinned, null: false, default: false
       t.timestamps
     end
+ 
 
     add_index :notes, :pinned
     add_index :notes, :created_at
